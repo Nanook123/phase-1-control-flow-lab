9a155211f951d9730036d7e4dcee73b256fdd2ef
Joshua Allford <joshallfordnanook@gmail.com>
2021-09-02 12:21:22 +0100

    Assignment Complete!

--- index.js
+++ index.js
@@ -1,11 +1,49 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(_someNumber) {
+  let feet = _someNumber;
+
+  if (feet <= 400) {
+    return `This one is on me!`;
+
+  } else if (feet >= 2000 && feet < 2500) {
+    return `I will gladly take your thirty bucks.`;
+
+  } else (feet >= 2500)
+  return `No can do.`;
+
+
+
+}
+scuberGreetingForFeet()
+
+function ternaryCheckCity(city) {
+
+
+
+  const cityMessage = city === 'NYC' ? "Ok, sounds good." : 'No go.';
+  return cityMessage
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+ternaryCheckCity()
+
+let tip1 = 'generous';
+let tip2 = 'not as generous';
+let tip3 = 'thanks for everything'
+
+function switchOnCharmFromTip(tips) {
+
+
+  switch (tips) {
+
+    case 'generous':
+      return "Thank you so much.";
+      break;
+    case 'not as generous':
+      return "Thank you.";
+      break;
+    case 'thanks for everything':
+      return "Bye."
+      break;
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+switchOnCharmFromTip()
